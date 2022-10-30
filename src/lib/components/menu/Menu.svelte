ca41e66977ff5e0b5dda87bf635117d99203c845
Ruan Pasta <ruan_augusto_pasta@hotmail.com>
2022-10-30 17:39:57 -0300

    Finished the first menu version

--- src/lib/components/menu/Menu.svelte
+++ src/lib/components/menu/Menu.svelte
@@ -5,74 +5,71 @@
 	import type { MenuItem } from './Menus'
 
 	let showMenu = false
-  export let menus: MenuItem[] = [] 
+	export let menus: MenuItem[] = []
 
-  const select = (menu) => {
-    menus.forEach((item) => {
-      if (item.name === menu.name) return item.selected = !item.selected
-      return item.selected = false
-    })
-    console.log(menus)
-  }
+	const closeExpandedMenu = (item: MenuItem): void => {
+		item.expanded = false
+		if (item.items?.length)
+      return item.items.forEach((item: MenuItem) => closeExpandedMenu(item))
+	}
 
-  const closeExpandedMenu = (item: MenuItem) => {
-    item.expanded = false
-    if (item.items?.length) return closeExpandedMenu(item.items)
-  }
-
-  const closeMenu = () => {
-    showMenu = false
-    menus.forEach(closeExpandedMenu)
-  }
+	const closeMenu = () => {
+		showMenu = false
+		menus.forEach(closeExpandedMenu)
+	}
 </script>
 
 <div class:gradient={showMenu}>
-  <div
-    class={classMap({
-      menu: true,
-      menu__list: showMenu,
-      'menu--open': showMenu
-    })}
-    on:mouseleave={() => closeMenu()}
-  >
-    <div>
-      <button type="button" title="menu" on:click={() => (showMenu = !showMenu)}>
-        <Menu size={30} />
-      </button>
-    </div>
+	<div
+		class={classMap({
+			menu: true,
+			menu__list: showMenu,
+			'menu--open': showMenu
+		})}
+		on:mouseleave={() => closeMenu()}
+	>
+		<div>
+			<button
+				type="button"
+				title="menu"
+				on:click={() => (showMenu = !showMenu)}
+			>
+				<Menu size={30} />
+			</button>
+		</div>
 
-    <ul class="menu__list__items">
-      {#if showMenu}
-        {#each menus as menu}
-          <MenuList {menu} {select} />
-        {/each}
-      {/if}
-    </ul>
-  </div>
+		<ul class="menu__list__items">
+			{#if showMenu}
+				{#each menus as menu}
+					<MenuList {menu} />
+				{/each}
+			{/if}
+		</ul>
+	</div>
 </div>
 
 <style lang="scss">
 	.menu {
 		@apply text-[color:var(--color-secondary)];
-    @apply rounded-b-[30px];
+		@apply rounded-b-[30px];
 
-    &__list {
-      @apply flex flex-col gap-4;
-      @apply bg-[color:var(--color-intermediate-background)];
-    }
+		&__list {
+			@apply flex flex-col gap-4;
+			@apply bg-[color:var(--color-intermediate-background)];
+		}
 
-    &--open {
-      @apply p-4 text-[color:var(--color-primary)];
-    }
-    
-    .menu__list__items {
-      @apply flex flex-col gap-4;
-    }
+		&--open {
+			@apply p-4 text-[color:var(--color-primary)];
+		}
+
+		.menu__list__items {
+			@apply flex flex-col gap-4;
+		}
 	}
 
-  .gradient {
-    @apply fixed top-0 left-0 w-full;
-    @apply pb-0.5 rounded-b-[30px];
-    @apply bg-gradient-to-r from-[color:var(--color-primary)] via-[color:var(--color-secondary)] to-[color:var(--color-tertiary)];
-  }
+	.gradient {
+		@apply fixed top-0 left-0 w-full;
+		@apply pb-0.5 rounded-b-[30px];
+		@apply bg-gradient-to-r from-[color:var(--color-primary)] via-[color:var(--color-secondary)] to-[color:var(--color-tertiary)];
+	}
 </style>
--- src/lib/components/menu/MenuList.svelte
+++ src/lib/components/menu/MenuList.svelte
@@ -1,85 +1,86 @@
 <script lang="ts">
 	import { classMap } from '$helpers/classMap'
-  import { onMount } from 'svelte'
+	import { onMount } from 'svelte'
 	import type { MenuItem } from './Menus'
 
-  let className = ''
-  export { className as class }
+	let className = ''
+	export { className as class }
 	export let menu: MenuItem
-  export let level = 0;
-  
-  let showHover = false
+	export let level = 0
 
-  const itemClicked = () => (menu.expanded = !menu.expanded)
-  
+	let showHover = false
+
+	const itemClicked = () => (menu.expanded = !menu.expanded)
 </script>
 
 <li
 	class={classMap({
-    [className]: true,
-    'menu': true,
-    'menu__submenu': level > 0,
-    'menu--expanded': menu.expanded && menu.items?.length
-  })}
+		[className]: true,
+		menu: true,
+		menu__submenu: level > 0,
+		'menu--expanded': menu.expanded && menu.items?.length
+	})}
 	on:click={itemClicked}
 	on:keydown={() => {}}
 >
 	{#if menu.icon}
 		<svelte:component this={menu.icon} />
 	{/if}
-  <div
-    class={classMap({
-      'menu__gradient': true,
-    })}
-    on:mouseenter={() => showHover = true}
-    on:mouseleave={() => showHover = false}
-  >
-    <a
-      href=""
-      class={classMap({
-        'menu__label': true,
-        'menu__label--hover': showHover 
-      })}>
-      {menu.name}
-    </a>
-  </div>
+	<div
+		class={classMap({
+			menu__gradient: true
+		})}
+		on:mouseenter={() => (showHover = true)}
+		on:mouseleave={() => (showHover = false)}
+	>
+		<a
+			href="/#"
+			class={classMap({
+				menu__label: true,
+				'menu__label--hover': showHover
+			})}
+		>
+			{menu.name}
+		</a>
+	</div>
 </li>
 
 {#if menu.expanded && menu.items}
 	{#each menu.items || [] as item}
-    <svelte:self menu={item} level={level + 1} class="ml-3" />
+		<svelte:self menu={item} level={level + 1} class="ml-3" />
 	{/each}
 {/if}
 
 <style lang="scss">
 	.menu {
 		@apply flex items-center gap-2 text-2xl cursor-pointer;
-    @apply text-[color:var(--color-text)];
+		@apply text-[color:var(--color-text)];
 
-    &__submenu {
-      @apply pl-8 ml-2.5;
-      @apply border-l-2;
-    }
+		&__submenu {
+			@apply pl-8 ml-2.5;
+			@apply border-l-2;
+		}
 
-    &--expanded {
-      @apply text-[color:var(--color-text)];
-    }
+		&--expanded {
+			@apply text-[color:var(--color-text)];
+		}
 	}
 
-  .menu__gradient {
-    @apply w-full ml-3 pb-0.5;
+	.menu__gradient {
+		@apply w-full ml-3 pb-0.5;
+    transition: background-color 1000ms linear;
 
-    &:active {
-      animation: ripple 600ms linear;
-      @apply bg-gradient-to-r from-[color:var(--color-primary)] via-[color:var(--color-secondary)] to-[color:var(--color-tertiary)];
-    }
-  }
+		&:active {
+			@apply bg-gradient-to-r from-[color:var(--color-primary)] via-[color:var(--color-secondary)] to-[color:var(--color-tertiary)];
+      
+		}
+	}
 
-  .menu__label {
-    @apply block bg-[color:var(--color-intermediate-background)];
+	.menu__label {
+		@apply block bg-[color:var(--color-intermediate-background)];
 
-    &--hover {
-      @apply text-[color:var(--color-text-highlighted)] font-black;
-    }
-  }
+		&--hover {
+			@apply text-[color:var(--color-text-highlighted)] font-black;
+		}
+	}
 </style>
