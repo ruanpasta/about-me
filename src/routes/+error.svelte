8cbb228ded588900414527a480533122b184d03c
Ruan Pasta <ruan_augusto_pasta@hotmail.com>
2022-12-05 13:25:19 -0300

    Update page 404

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,2 +1,9 @@
-<!-- TODO: Create a 404 notfound page -->
-Pagina nao encontrada
+<script>
+	import { t } from '$lib/translations'
+
+</script>
+
+<div class="text-center text-2xl mt-[20%]">
+  <span class="font-bold text-red/80">404</span>
+  {$t('common.pageNotFound')}
+</div>
